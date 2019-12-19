05f3c51272415dc1595dbefb931112bd41c1af3d
Tom McKernan <tmeaglei@gmail.com>
2019-12-19 13:27:42 -0600

    version 0.3

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate clap;
 
 use clap::{App, AppSettings};
-use std::process::{Command, Stdio};
+use std::process::{Command, Stdio, exit};
 use std::time::SystemTime;
 
 fn main() {
@@ -12,14 +12,51 @@ fn main() {
         .get_matches();
 
     let command: Vec<_> = matches.values_of("COMMAND").unwrap().collect();
+    let disable_output = matches.is_present("disable-output");
 
-    let start_time = SystemTime::now();
+    let num_times_res = matches
+        .value_of("times")
+        .unwrap_or("1")
+        .parse::<i32>();
+    let num_times = match num_times_res {
+        Ok(num) => num,
+        Err(_) => {
+            eprintln!("That is not an integer");
+            exit(1);
+        }
+    };
 
+    let mut times_vec: Vec<u128> = vec![];
+
+    for _ in 0..num_times {
+        let start_time = SystemTime::now();
+        run_command(command.clone(), disable_output);
+        match start_time.elapsed() {
+            Ok(time) => {
+                times_vec.push(time.as_millis())
+            }
+            Err(e) => eprintln!("{:?}", e)
+        }
+    }
+
+    for elapsed in times_vec {
+        println!("{}", elapsed);
+    }
+}
+
+
+fn run_command(command: Vec<&str>, disable_output: bool) {
     match Command::new(command[0])
         .args(&command[1..])
-        .stdin(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
+        .stdin(if disable_output {
+            Stdio::null()
+        } else { Stdio::inherit() })
+        .stderr(if disable_output {
+            Stdio::null()
+        } else { Stdio::inherit() })
+        .stdout(if disable_output {
+            Stdio::null()
+        } else { Stdio::inherit() })
         .spawn() {
         Ok(mut child) => {
             child.wait().unwrap();
@@ -27,10 +64,5 @@ fn main() {
         Err(_) => {
             eprintln!("This command does not exist.")
         }
-    }
-
-    match start_time.elapsed() {
-        Ok(time) => println!("{}", time.as_millis()),
-        Err(e) => eprintln!("{:?}", e)
     }
 }
